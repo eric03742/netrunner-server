84e8f8665b9b8afc6c857cbc1d39b2d8c57c5b24
Eric03742 <eric03742@foxmail.com>
2025-05-13 18:36:49 +0800

    ➕ add 'serve-static' module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from "@nestjs/common";
+import { ServeStaticModule } from "@nestjs/serve-static";
+
 import { CatsModule } from "./cats/cats.module";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 @Module({
-    imports: [CatsModule],
+    imports: [
+        CatsModule,
+        ServeStaticModule.forRoot({
+            rootPath: "static",
+            serveRoot: "/static",
+            serveStaticOptions: {
+                cacheControl: true,
+                immutable: true,
+                maxAge: 864000000,
+            },
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
